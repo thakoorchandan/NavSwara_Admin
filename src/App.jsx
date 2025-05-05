460215b53dc4323025736986495849dd8c59397a
thakoor <thakoor@interviewdesk.in>
2025-05-05 15:46:08 +0530

    added default route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/navbar"
 import Sidebar from "./components/sidebar"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Add from './pages/add'
 import List from './pages/list'
 import Orders from './pages/orders'
@@ -34,6 +34,7 @@ const App = () => {
       <Sidebar/>
       <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
         <Routes>
+        <Route path='/' element={<Navigate to='/add' replace />} />
           <Route path='/add' element={<Add token={token}/>}/>
           <Route path='/list' element={<List  token={token}/>}/>
           <Route path='/orders' element={<Orders  token={token}/>}/>
